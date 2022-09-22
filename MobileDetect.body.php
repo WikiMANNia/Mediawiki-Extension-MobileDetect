69873bc0ec86beffbd731b975ff5baa931613449
WikiMANNia <55834825+WikiMANNia@users.noreply.github.com>
2022-09-22 16:38:55 +0200

    Version 3.0

--- MobileDetect.body.php
+++ MobileDetect.body.php
@@ -2,6 +2,21 @@
 
 class MobileDetect {
 
+	private static $_isMobile;
+
+	private static function getIsMobile() {
+		if ( self::$_isMobile === null ) {
+			self::$_isMobile = self::isMobile();
+		}
+
+		return self::$_isMobile;
+	}
+
+	public static function addModule( &$output ) {
+		$output->addModules( 'ext.MobileDetect' );
+		return true;
+	}
+
 	public static function setParserHook( &$parser ) {
 		$parser->setHook( 'nomobile', 'MobileDetect::nomobile' );
 		$parser->setHook( 'mobileonly', 'MobileDetect::mobileonly' );
@@ -9,17 +24,25 @@ class MobileDetect {
 	}
 
 	public static function nomobile( $input, array $args, Parser $parser, PPFrame $frame ) {
-		if ( self::isMobile() ) {
+		if ( self::getIsMobile() ) {
+			// Remove content within tag <nomobile>
+			$parser->recursiveTagParse( $input, $frame );
 			return '';
+		} else {
+			// Only remove tag <nomobile>
+			return $parser->recursiveTagParse( $input, $frame );
 		}
-		return $parser->recursiveTagParse( $input );
 	}
 
 	public static function mobileonly( $input, array $args, Parser $parser, PPFrame $frame ) {
-		if ( self::isMobile() ) {
-			return $parser->recursiveTagParse( $input );
+		if ( self::getIsMobile() ) {
+			// Only remove tag <mobileonly>
+			return $parser->recursiveTagParse( $input, $frame );
+		} else {
+			// Remove content within tag <mobileonly>
+			$parser->recursiveTagParse( $input, $frame );
+			return '';
 		}
-		return '';
 	}
 
 	public static function isMobile() {
@@ -63,12 +86,12 @@ class MobileDetect {
 
 		// Others
 		// Check if any of the values listed create a match on the user agent - these are some of the most common terms used in agents to identify them as being mobile devices - the i at the end makes it case insensitive
-		if ( preg_match( '/(mini 9.5|vx1000|lge |m800|e860|u940|ux840|compal|wireless| mobi|ahong|lg380|lgku|lgu900|lg210|lg47|lg920|lg840|lg370|sam-r|mg50|s55|g83|t66|vx400|mk99|d615|d763|el370|sl900|mp500|samu3|samu4|vx10|xda_|samu5|samu6|samu7|samu9|a615|b832|m881|s920|n210|s700|c-810|_h797|mob-x|sk16d|848b|mowser|s580|r800|471x|v120|rim8|c500foma:|160x|x160|480x|x640|t503|w839|i250|sprint|w398samr810|m5252|c7100|mt126|x225|s5330|s820|htil-g1|fly v71|s302|-x113|novarra|k610i|-three|8325rc|8352rc|sanyo|vx54|c888|nx250|n120|mtk |c5588|s710|t880|c5005|i;458x|p404i|s210|c5100|teleca|s940|c500|s590|foma|samsu|vx8|vx9|a1000|_mms|myx|a700|gu1100|bc831|e300|ems100|me701|me702m-three|sd588|s800|8325rc|ac831|mw200|brew |d88|htc\/|htc_touch|355x|m50|km100|d736|p-9521|telco|sl74|ktouch|m4u\/|me702|8325rc|kddi|phone|lg |sonyericsson|samsung|240x|x320|vx10|nokia|sony cmd|motorola|up.browser|up.link|mmp|symbian|smartphone|midp|wap|vodafone|o2|pocket|kindle|mobile|psp|treo)/i', $user_agent ) ) {
+		if ( preg_match( '/(mini 9.5|vx1000|lge|m800|e860|u940|ux840|compal|wireless|mobi|ahong|lg380|lgku|lgu900|lg210|lg47|lg920|lg840|lg370|sam-r|mg50|s55|g83|t66|vx400|mk99|d615|d763|el370|sl900|mp500|samu3|samu4|vx10|xda_|samu5|samu6|samu7|samu9|a615|b832|m881|s920|n210|s700|c-810|_h797|mob-x|sk16d|848b|mowser|s580|r800|471x|v120|rim8|c500foma:|160x|x160|480x|x640|t503|w839|i250|sprint|w398samr810|m5252|c7100|mt126|x225|s5330|s820|htil-g1|fly v71|s302|-x113|novarra|k610i|-three|8325rc|8352rc|sanyo|vx54|c888|nx250|n120|mtk|c5588|s710|t880|c5005|i;458x|p404i|s210|c5100|teleca|s940|c500|s590|foma|samsu|vx8|vx9|a1000|_mms|myx|a700|gu1100|bc831|e300|ems100|me701|me702m-three|sd588|s800|8325rc|ac831|mw200|brew|d88|htc\/|htc_touch|355x|m50|km100|d736|p-9521|telco|sl74|ktouch|m4u\/|me702|8325rc|kddi|phone|lg|sonyericsson|samsung|240x|x320|vx10|nokia|sony cmd|motorola|up.browser|up.link|mmp|symbian|smartphone|midp|wap|vodafone|o2|pocket|kindle|mobile|psp|treo)/i', $user_agent ) ) {
 			return true; // Mobile
 		}
 
 		// Detect WAP Support
-		if ( ( strpos( $http_accept, 'text/vnd.wap.wml') > 0 ) || ( strpos( $http_accept, 'application/vnd.wap.xhtml+xml' ) > 0 ) ) {
+		if ( ( strpos( $http_accept, 'text/vnd.wap.wml' ) > 0 ) || ( strpos( $http_accept, 'application/vnd.wap.xhtml+xml' ) > 0 ) ) {
 			return true; // Mobile
 		}
 
@@ -83,7 +106,7 @@ class MobileDetect {
 			return true;
 		}
 
-		// If all checks pass, return false
+		// If all checks pass, we're not in a mobile device
 		return false;
 	}
 }
--- MobileDetect.php
+++ MobileDetect.php
@@ -2,11 +2,11 @@
 
 $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'MobileDetect',
-	'version' => 2.0,
+	'version' => '3.0',
 	'license-name' => 'GPL-2.0+',
 	'descriptionmsg' => 'mobiledetect-desc',
 	'author' => array( 'Matthew Tran', 'Luis Felipe Schenone' ),
-	'url' => 'http://www.mediawiki.org/wiki/Extension:MobileDetect',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:MobileDetect',
 );
 
 $wgExtensionMessagesFiles['MobileDetect'] = __DIR__ . '/MobileDetect.i18n.php';
@@ -14,7 +14,18 @@ $wgMessagesDirs['MobileDetect'] = __DIR__ . '/i18n';
 
 $wgAutoloadClasses['MobileDetect'] = __DIR__ . '/MobileDetect.body.php';
 
+$wgHooks['BeforePageDisplay'][] = 'MobileDetect::addModule';
 $wgHooks['ParserFirstCallInit'][] = 'MobileDetect::setParserHook';
+
+$wgResourceModules['ext.MobileDetect'] = array(
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'MobileDetect',
+);
+if ( MobileDetect::isMobile() ) {
+	$wgResourceModules['ext.MobileDetect']['styles'] = 'MobileDetect.mobileonly.css';
+} else {
+	$wgResourceModules['ext.MobileDetect']['styles'] = 'MobileDetect.nomobile.css';
+}
 
 // Backwards compatibility
 function mobiledetect() {
